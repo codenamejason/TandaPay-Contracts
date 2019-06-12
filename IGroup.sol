941e25021403b4ed2e0fdcbd9fd6347039aee443
jp4g <gilcrejp@miamioh.edu>
2019-06-12 13:57:34 -0500

    Draft 2

--- IGroup.sol
+++ IGroup.sol
@@ -1,36 +1,64 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
-import "./TandaLibrary.sol";
+import 'http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Secondary.sol';
 
 /**
  * @author blOX Consulting LLC.
- * Date: 06.09.2019
+ * Date: 06.12.2019
  * Interface for TandaGroups
- * For Secretary Group management/ Policyholder Subgroup management 
+ * User services: Policyholder and Secretary
+ * Groups are deployed for secretaries by TandaPayService
  **/
-contract IGroup {
+contract IGroup is Secondary {
     
     ///EVENTS///
-    event created(address _secretary);
-    event policyholderAdded(address _secretary, address _policyholder);
-    event policyholderRemoved(address _secretary, address _policyholder);
-    event subgroupChange(address _secretary, address _policyholder);
-    
-    ///MAPPING///
-    mapping(address => TandaLibrary.group) groups;
-    mapping(address => address) policyholderToGroup;
-    
-    ///FUNCTIONS///
+    event policyholderAdded(address _policyholder);
+    event policyholderRemoved(address _policyholder);
+    event subgroupChange(address _policyholder);
+    event premiumPaid(address _policyholder);
+    event locked(uint timelock);
+    event refund();
+    event activePeriod();
+    event postPeriod();
+    event claimOpened(address _policyholder);
+    event claimRejected(address _policyholder);
+    event claimApproved(address _policyholder);
+    event defected(address _defector);
 
-    /**
-     * @dev sender must be approved to be Secretary
-     * @dev sender must not already have an existing groups
-     * Create a new Tanda groups
-     * @param _premium the premium paid by the Policyholder
-     * @dev TandaLibrary.MIN_PREMIUM <= _premium <= TandaLibrary.MAX_PREMIUM
-     **/
-    function create(uint8 _premium) public;
+    ///MAPPINGS///
+    mapping(address => uint8) policyholders;
+    mapping(uint16 => period) periods;
+
+    ///ADDRESSES///
+    address this;
+    address secretary;
+    address[] activeParticipants;
+
+    ///ENUMERATIONS///
+    enum policyholderState {UNPAID, PAID, DEFECTED}
+    enum periodState {PRE, ACTIVE, POST}
+    enum claimState {REJECTED, OPEN, ACCEPTED}
+
+    ///INTEGERS///
+    uint8 premium;
+    uint8 size;
+    uint16 periodIndex;
+    uint timelock;
+
+    ///STRUCTS///
+    struct claim {
+        address policyholder;
+        claimState state;
+    }
     
+    struct period {
+        periodState state;
+        claim[] claims;
+        mapping(address => policyholderState) policyholders;
+        mapping(uint8 => uint8) defectionCount;
+    }
+
+    ///FUNCTIONS///
     /**
      * @dev modifier onlySecretary
      * Add a Policyholder to the group
@@ -53,5 +81,46 @@ contract IGroup {
      * @param _subgroup the subgroup being switched to
      **/
     function changeSubgroup(address _policyholder, uint8 _subgroup) public;
+
+    /**
+     * @dev modifier onlyPolicyholder
+     * Policyholder pays their premium for the month
+     **/
+    function payPremium() public;
+    
+    /**
+     * @dev modifier onlySecretary, prePeriod
+     * Secretary activates the 27 day insurance contract
+     * Premiums are locked into this group's address
+     * @dev restrictions on participant #?
+     **/
+    function startPeriod() public;
+    
+    /**
+     * @dev modifier onlyPolicyholder, activePeriod
+     * Policyholder opens a new claim
+     **/
+    function openClaim() public;
+    
+    /**
+     * @dev function onlySecretary, activePeriod
+     * Secretary rejects a claim
+     * @param _index the index of the claim within the period
+     **/
+    function rejectClaim(uint _index) public;
+    
+    /**
+     * @dev function onlySecretary, activePeriod
+     * Secretary approves a claim
+     * @param _index the index of the claim being approved
+     **/
+    function approveClaim(uint _index) public;
+    
+    /**
+     * @dev function onlyPolicyholder, postPeriod
+     * Policyholder defects from the Tanda Group
+     * @dev automatically remit and eject
+     **/
+    function defect() public;
 }
 
--- ITandaPayService.sol
+++ ITandaPayService.sol
@@ -1,56 +1,114 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
-import "./TandaLibrary.sol";
+import 'http://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
 
 /**
  * @author blOX Consulting LLC.
- * Date: 6.8.19
- * Interface for main TandaPay service. Would store references to all of the other contracts.
+ * Date: 6.12.19
+ * Interface for main TandaPay service
+ * Factory contract for groups
+ * CRON/ Admin functionality
  **/
 contract ITandaPayService {
-    
+
     ///EVENTS///
-    event adminAdded(address _added);
-    event adminRemoved(address _removed);
-    event secretaryApproved(address _approved);
+    event adminApproved(address _approved);
+    event adminRevoked(address _revoked);
+    event groupCreated(address _group, address _secretary);
     event secretaryRevoked(address _revoked);
-    
+    event remitted(address _group);
+
     ///MAPPING///
     mapping(address => bool) admin;
-    mapping(address => bool) approvedSecretaries;
-    
+    mapping(address => address) approvedSecretaries;
+    mapping(address => address) approvedPolicyholders;
+
     ///CONTRACTS///
-    //Group GroupContract;
-    //Period PeriodContract;
-    //Cron CronContract;
-    
+    IERC20 Dai;
+
+    ///ENUMERATIONS///
+    enum policyholderState {UNPAID, PAID, DEFECTED}
+    enum periodState {PRE, ACTIVE, POST}
+    enum claimState {REJECTED, OPEN, ACCEPTED}
+
+    ///INTEGERS///
+    uint8 constant MIN_PREMIUM = 5;
+    uint8 constant MAX_PREMIUM = 50;
+
+    ///ADDRESSES///
+    address constant kovan = 0xc4375b7de8af5a38a93548eb8453a498222c4ff2;
+    address constant mainnet = 0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359;
+    address[] groupContracts;
+
     ///FUNCTIONS///
-    
     /**
      * @dev modifier onlyAdmin
-     * Add an admin 
+     * Add an admin
      * @param _to the address being whitelisted
+     * @dev add timelock
      **/
     function addAdmin(address _to) public;
-    
+
     /**
      * @dev modifier onlyAdmin
      * Remove an admin
      * @param _from the address being blacklisted
+     * @dev add timelock
      **/
     function removeAdmin(address _from) public;
-     
+
     /**
      * @dev modifier onlyAdmin
-     * Approve a secretary to create a group
+     * @dev modifier validPremium
+     * Approve a secretary and create a group
+     * @param _to the address being given a secretary role
+     * @param _group the address of the constructed group
      **/
-    function addSecretary(address _to) public;
-      
+    function addGroup(address _to, uint8 premium) public returns (address _group);
+
     /**
      * @dev modifier onlyAdmin
-     * Remove secretary group privledge
+     * Remove secretary
+     * @param _from the address being revoked the secretary role
+     * @dev Ownership of Tanda group assigned to this contract
      **/
     function removeSecretary(address _from) public;
-    
+
+    /**
+     * @dev modifier onlyAdmin
+     * Remit all groups available to be remitted
+     * @dev gas-heavy failsafe, processAll should be done 'manually' by web3
+     **/
+    function processAll() public;
+
+    /**
+     * @dev modifier onlyAdmin
+     * @dev modifier unlocked
+     * Remit a single group assuming it is not timelocked
+     * @param _group the address of the group being remitted
+     **/
+    function remitGroup(address _group) public;
+
+    /**
+     * @dev called by remitGroup()
+     * Pays out defections and strips claims in toxic subgroups
+     * @return _group the address of the group being remitted
+     **/
+    function processDefections() internal returns (address _group);
+
+    /**
+     * @dev called by remitGroup()
+     * Process all of the remaining claims
+     * @param _group the address of the group being remitted
+     * @param _payout the value being paid out in the claim
+     **/
+    function processClaims(address _group, uint16 _payout) internal;
+
+    /**
+     * @dev called by remitGroup()
+     * If possible, refund an equal portion of the Dai to each policyholder
+     * @param _group the group being refunded
+     **/
+    function processRefunds(address _group) internal;
 }
 
