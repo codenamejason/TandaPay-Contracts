4f7ff73f099efbfe089292f710e052f52a48c3d3
cylon56 <michaelnlewellen@gmail.com>
2019-10-23 12:12:15 -0500

    added rinkeby network configurations

--- truffle-config.js
+++ truffle-config.js
@@ -10,10 +10,20 @@ module.exports = {
 
   networks: {
     development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    jack_dev: {
       provider: () => { return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545", 0, 100) },
       network_id: "*",
       gasLimit: 7000000
-    }  
+    },
+    rinkeby: {
+      provider: () => { return new HDWalletProvider(process.env.MNEMONIC, process.env.API_RINKEBY,  0, 100) },
+      network_id: 4,
+      confirmations: 2,
+    },
   },
 
   mocha: {
@@ -22,7 +32,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      verion: "0.5.10",
+      verion: "0.5.2",
       settings: {
         optimizer: {
           enabled: true,
